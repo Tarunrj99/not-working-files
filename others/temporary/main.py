36167cc80010a4c95c9a14c6aa3d86dc66ed157e
Tarunrj99 <86702050+Tarunrj99@users.noreply.github.com>
2025-03-17 19:00:46 +0530

    Update main.py

--- others/temporary/main.py
+++ others/temporary/main.py
@@ -14,9 +14,9 @@ signing_secret = get_secret("tarun-signing-secret")
 app = App(signing_secret=signing_secret, token=slack_token)
 handler = SlackRequestHandler(app)
 if "users" not in globals():
-    users = {
-        "U050DRWLZLG": "spa-token",
-        "U07BC7QCEQM": "tpa-token"
+    users = { 
+        "U050DRWLZLG": "tpa-token",
+        "U07BC7QCEQM": "spa-token"
     }
 emoji_actions = {
     "ok": {"action": "approve", "message": "approve kr diya"}, #{user_name}
